b2501037ff7643051bf0a33ef577c5a22b5eee5f
StefanoMela <stefano.mela25@gmail.com>
2024-06-12 15:29:28 +0200

    first commit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,26 @@
+import Button from "../Button/Button";
+import cardStyle from "./Card.module.css";
+
+function Card() {
+  return (
+    <>
+      <div className={cardStyle.card}>
+        <img src="https://picsum.photos/600/500" alt="" />
+        <div className={cardStyle.cardBody}>
+        <h3 className={cardStyle.textLeft}>Titolo</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
+          odit quos sint hic ut! Voluptatum iure omnis dolores assumenda
+          repudiandae doloribus nostrum impedit itaque, error beatae rerum
+          labore at, iste dicta sequi possimus aut corrupti, distinctio ut
+          placeat veniam hic. Voluptas eos eius saepe dolorum vero ab
+          repudiandae, praesentium fugiat!
+        </p>
+        <Button />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Card;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import Card from '../Card/Card';
+import './Main.css';
+
+function Main() {
+  return (
+    <>
+    <div className="main">
+      <h2>Il mio blog</h2>
+      <Card />
+    </div>
+    </>
+  );
+}
+
+export default Main;
